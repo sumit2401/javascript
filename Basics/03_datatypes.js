aec8f5f6c7cd8155e72e6566cbd79a960649abe4
Sumit Patel <76784329+sumit2401@users.noreply.github.com>
2023-12-14 17:33:48 +0530

    Update 03_datatypes.js

--- Basics/03_datatypes.js
+++ Basics/03_datatypes.js
@@ -20,3 +20,25 @@ non-primitve-
 object
 
 */
+//for furthur details read the ecma standard files
+
+//covvert from one datatype to other
+ let score = "33"
+ let scoreinnumber = Number(score)
+ console.log(scoreinnumber)
+ console.log(typeof scoreinnumber)
+
+ let score11="33aa"
+ let score11innumber = Number(score11)
+ console.log(typeof score11innumber)
+ console.log(score11innumber) // => this will give you NaN that means not a number
+
+ //if the value is null then the datatype will be object
+ let score1= null
+ console.log(typeof score1);
+
+ //if the value is undefined then the datatype is also undefined
+ let score2;
+ console.log(typeof score2)
+
+
