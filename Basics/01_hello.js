920b9af0db6c29e4465802c1a8828de45082a965
Sumit Patel <76784329+sumit2401@users.noreply.github.com>
2023-12-14 11:25:59 +0000

    basics of js

---
+++ Basics/01_hello.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+console.log("hello this is sumit patel, start learning javascript basic")
+
+//in javascript ";" semicolon are not mandatory, semicolon are usually use to break the line but in js if we write the code in new line it will treat  differently 
+
+/* console.log("hello") console.log("this is sumit")
+this is wrong way to write in here the compiler give us error bcoz there is two console.log in one line 
+
+
+ console.log("hello"); console.log("this is sumit")
+ this is correct bcoz we add semicolon between them so now the line is break between these console now they will treat differently 
+
+ but again never write code like this because as a coder we have to code which is readable and less confusing
+*/
