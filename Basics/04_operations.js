66b617a2b9c9c403aa55d566ee5110cde2e19461
Sumit Patel <76784329+sumit2401@users.noreply.github.com>
2023-12-15 19:38:00 +0530

    config done

---
+++ Basics/04_operations.js
@@ -0,0 +1,38 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)//2 raise to the power 2
+console.log(2**3)
+console.log(2^3)
+console.log(2/2)
+console.log(2%2)
+
+let str1="Hello"
+let str2=" world"
+let str3 = str1+str2
+console.log(str3)
+//now some confusing operation
+
+console.log("1"+2)
+console.log(1+"2")
+
+//in adove example if the string is in the first place then in js it will automatically convert every value into strings
+console.log(1+2+"2")// but if the string is in the last place then the firstly the value will add or subract and concat with string
+
+console.log("1"+2+2)
+
+//prefix and postfix addition and substraction
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+
+let x = 3;
+const y = x++; //first use then increment so acc to this first the value of x is use in y then increment in x
+
+console.log(X=x, Y=y); 
+
+let b = 3;
+const a = ++b; //first incrementthen use 
+
+console.log(X=b, Y=a); 
