8dd242bcbe80acc7d95077fed9b81eeaa392019b
Sumit Patel <76784329+sumit2401@users.noreply.github.com>
2023-12-18 22:15:54 +0530

    config done

---
+++ Basics/08_nums_and_math.js
@@ -0,0 +1,23 @@
+// const score = 400
+// console.log(score)
+
+// const Balance =new Number(100)
+// console.log(Balance);
+
+// //now some int methods
+
+// console.log(Balance.toString().length);
+
+// const number = 743.873489
+// console.log(number.toFixed(2));//fixed the value of decimal in numbers
+// console.log(number.toPrecision(5));//provide the precise(exact) value of number with or without roundind of
+
+// const hunderds=10000000
+// console.log(hunderds.toLocaleString("en-IN")); //convert the number according to indian standards
+
+//+++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+console.log(Math.abs(-4));//convert negative number into positive
+console.log(Math.acos(0));//return the inverted cos value
+
+console.log(Math.fround(324.32));
