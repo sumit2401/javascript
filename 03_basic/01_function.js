c7de6b171831633257c992f050959fde02a78de0
Sumit Patel <sp1440793@gmail.com>
2023-12-31 12:03:02 +0530

    update

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -28,8 +28,10 @@ const result = addTwoNumbers(2, 2);
 // console.log("Result: ", result); //the value we pass while calling function is argument.
 //ex- (num1,num2) are parameters and (2,2) is arguments
 
-function loginUserMessage(username) {
+function loginUserMessage(username = "Sam") {
+  //we can also pass the default value
   if (!username) {
+    //"!" convert true to false and vica versa
     console.log("Please enter your username");
     return;
   }
@@ -37,3 +39,41 @@ function loginUserMessage(username) {
 }
 
 console.log(loginUserMessage());
+console.log(loginUserMessage("Sumit"));
+
+//for shopping cart
+
+function calculateCartPrice(...num1) {
+  //"..." is used in both rest and spread operator .. spread use to concat multiple array or object on the otherhand rest is used if we wamt to bundle up the  multiple input like in shopping cart
+  return num1;
+}
+
+// console.log(calculateCartPrice(100, 200, 3000, 43534));
+
+const user = {
+  userName: "Sumit Patel",
+  price: 500,
+};
+
+function handleObject(anyobject) {
+  // pass anyobject if we want object as parameters
+  console.log(
+    `UserName is ${anyobject.userName} amd price is ${anyobject.price}`
+  );
+}
+
+handleObject(user);
+
+//also pass the object like this
+handleObject({
+  userName: "Sumit Patel",
+  price: 500,
+});
+
+const myArray = [200, 400, 500, 700];
+
+function findSecondValue(getArray) {
+  console.log(getArray[1]);
+}
+
+findSecondValue(myArray);
