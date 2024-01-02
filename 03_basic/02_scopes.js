8b92fa18f0e0596cefa0de1931e9204418d969c5
Sumit Patel <sp1440793@gmail.com>
2024-01-02 20:27:42 +0530

    config done

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -23,3 +23,36 @@ console.log(a); // we find error in this
 //the global scope is window/ browser is different than nodeJs environment  scope
 
 //the global scope in window become methods and properties of window a
+
+//nested scope
+
+//let me explain with the help of example-" A small child, if asking for ice cream from their parents, would feel good, but if parents ask for ice cream from that small child, it wouldn't be nice, right?- so in here parent in function one and children is function two"
+
+function one() {
+  const userName = "Sumit";
+
+  //child function can access parent variable but parent function can't access children variable
+  function two() {
+    const website = "youtube";
+    console.log(userName);
+  }
+  // console.log(website);
+
+  two();
+}
+
+one();
+
+//************ intresting **************/ concept of hoisting
+
+//hoisting means when then variable and function call before the initialization of variable and function ex-
+console.log(addOne(5));
+function addOne(num) {
+  return num + 1;
+}
+// //
+// console.log(addTWo(5));// show error because this function is bounded inside the variable and we cant access variable before initialisation
+
+const addTWo = function (num) {
+  return num + 2;
+};
